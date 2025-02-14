397103ee8fa5149c5eafaac7087db1f43c9737e0
PierluCarlBcSoft <140423853+PierluCarlBcSoft@users.noreply.github.com>
2025-02-14 12:45:22 +0100

    Formatted dto dates to dd-MM-yyyy HH:mm

--- src/main/java/com/adesso/awesome_pizza/orders/dto/OrderDTO.java
+++ src/main/java/com/adesso/awesome_pizza/orders/dto/OrderDTO.java
@@ -3,12 +3,15 @@ package com.adesso.awesome_pizza.orders.dto;
 import java.time.LocalDateTime;
 
 import com.adesso.awesome_pizza.orders.model.enumeration.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class OrderDTO {
 	
 	public Long id;
     public String description;
     public OrderStatus status;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     public LocalDateTime insertDate;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     public LocalDateTime updateDate;
 }
